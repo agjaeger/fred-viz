5e41da0d5b56c3e7381112455ae70021eb63fbc9
Alex Jaeger <agjaeger@ualr.edu>
2017-03-03 23:32:40 -0600

    added some comments

--- tools/preprocess.py
+++ tools/preprocess.py
@@ -27,21 +27,26 @@ def calcCorrelation(key1, key2):
 		dataList1.append(float(data_dict[k][key1]))
 		dataList2.append(float(data_dict[k][key2]))
 
+	# correcoef returns 2x2 matrix, row 0 col 1 represents cor(a, b)
 	return np.corrcoef(dataList1, dataList2)[0][1]
 
+# read every raw data file
 for data_r in raw_files:
 	with open(data_dir + data_r + '.csv', 'r') as data_read:
 		reader = csv.DictReader(data_read)
 		columns = reader.fieldnames
-			
+		
+		# zip the data together so it is unified
 		for row in reader:
 			if row[columns[0]] not in data_dict:
 				data_dict[row[columns[0]]] = {}
 				
 			data_dict[row[columns[0]]][columns[1]] = row[columns[1]]
 	
+		# save the zipped data for future viz
 		writeout('zipped.json', data_dict)
-	
+
+# generate a list of perms so i can correlate each one
 perms = list(itertools.permutations(raw_files, 2))
 for raw in raw_files:
 	perms.append([raw, raw])
