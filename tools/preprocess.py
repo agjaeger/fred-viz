05e77bc7def19a5f57d7c984183266ef50fdc094
Alex Jaeger <agjaeger@ualr.edu>
2017-03-03 23:19:50 -0600

    created json processed files and added correlation calculation scripts

--- tools/preprocess.py
+++ tools/preprocess.py
@@ -2,21 +2,35 @@
 
 import csv
 import json
+import itertools
+import numpy as np
 
 data_dir = 'data/'
 
-raw_files = ['EXPCH.csv', 'EXPGE.csv', 'EXPMX.csv', 'IMPCH.csv', 'IMPGE.csv', 'IMPMX.csv']
-output_files = ['zipped.json', 'correlated.json']
+raw_files = ['EXPCH', 'EXPGE', 'EXPMX', 'IMPCH', 'IMPGE', 'IMPMX']
 
 data_dict = {}
+correlated_data = {}
 
 def writeout(filename, dictionary):
-	with open(data_dir + filename, 'w') as data_out:
-		json_out = json.dumps(dictionary)
-		print(json_out)
+	json_out = json.dumps(dictionary)
 	
+	f = open(data_dir + filename, 'w')
+	f.write(json_out)
+	f.close()
+	
+def calcCorrelation(key1, key2):
+	dataList1 = []
+	dataList2 = []
+	
+	for k, v in data_dict.items():
+		dataList1.append(float(data_dict[k][key1]))
+		dataList2.append(float(data_dict[k][key2]))
+
+	return np.corrcoef(dataList1, dataList2)[0][1]
+
 for data_r in raw_files:
-	with open(data_dir + data_r, 'r') as data_read:
+	with open(data_dir + data_r + '.csv', 'r') as data_read:
 		reader = csv.DictReader(data_read)
 		columns = reader.fieldnames
 			
@@ -26,5 +40,14 @@ for data_r in raw_files:
 				
 			data_dict[row[columns[0]]][columns[1]] = row[columns[1]]
 	
-		writeout(output_files[0], data_dict)
+		writeout('zipped.json', data_dict)
 	
+perms = list(itertools.permutations(raw_files, 2))
+for raw in raw_files:
+	perms.append([raw, raw])
+	
+for perm in perms:
+	correlated_data[perm[0] + '-' + perm[1]] = calcCorrelation(perm[0], perm[1])
+
+writeout('correlated.json', correlated_data)
+
